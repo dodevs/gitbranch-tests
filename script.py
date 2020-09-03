5d244dfb81e4698cf582bdd47deeee2ed5c9575b
dodevs <douglass.sousa@outlook.com.br>
2020-09-03 10:19:12 -0300

    Perceint function

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
+def perceint(a, b):
+    return a/100 * b
+
 def sum(a,b):
     return a + b
 
 def main():
     print("Main function")
     print(f"Sum of 2 and 5 is {sum(2,5)}")
+    print(f"15% of 200 is {perceint(15, 200)}")
 
 if __name__ == "__main__":
     main()
