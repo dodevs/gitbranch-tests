5bf4dfa6db131305754d2d678fe953ace4b09b0d
dodevs <douglass.sousa@outlook.com.br>
2020-09-03 10:14:58 -0300

    Div function

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
+def div(a,b):
+    return a / b
+
 def sum(a,b):
     return a + b
 
 def main():
     print("Main function")
     print(f"Sum of 2 and 5 is {sum(2,5)}")
+    print(f"Div of 10 by 5 is {div(10,5)}")
 
 if __name__ == "__main__":
     main()
